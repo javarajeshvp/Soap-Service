5d0827bd6efd829f217e70111991ccb1e1689a9f
javarajeshvp <>
2019-08-29 22:46:02 -0700

    Code Cleanup

--- src/com/vp/soapservice/ServicePublisher.java
+++ src/com/vp/soapservice/ServicePublisher.java
@@ -2,11 +2,9 @@ package com.vp.soapservice;
 
 import javax.xml.ws.Endpoint;
 
-public class Publisher {
+public class ServicePublisher {
 
 	public static void main(String[] args) {
 		Endpoint.publish("http://localhost:8082/SoapService/services/ProductService", new ProductService());
-
 	}
-
 }
